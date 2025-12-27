6d53733613cc8083ccabfab9915e122187f666f8
linhken <silinh66@gmail.com>
2025-12-28 03:03:44 +0700

    ok

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,10 +1,11 @@
 module.exports = {
     apps: [
         {
-            name: "vibita tracking",
-            script: "npm",
-            args: "run start",
-            interpreter: "none", // <--- THÊM DÒNG QUAN TRỌNG NÀY
+            name: "app.vibita",
+            // Thay vì gọi npm, ta gọi thẳng cmd của Windows
+            script: "C:\\Windows\\System32\\cmd.exe",
+            // Truyền lệnh "npm run start" vào cho cmd xử lý
+            args: "/c npm run start",
             env: {
                 PORT: 5188,
                 NODE_ENV: "production"
