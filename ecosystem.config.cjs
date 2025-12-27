2bba44a2bf3850e7895dcb02234380ce3023424d
linhken <silinh66@gmail.com>
2025-12-28 03:15:41 +0700

    ok

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,9 +2,15 @@ module.exports = {
     apps: [
         {
             name: "app.vibita",
-            script: "npm",
-            args: "run start",
-            interpreter: "none", // <--- THÊM DÒNG QUAN TRỌNG NÀY
+            // Trỏ thẳng vào file chạy của remix-serve trong node_modules
+            script: "./node_modules/.bin/remix-serve.cmd",
+
+            // Đường dẫn đến file build của bạn
+            args: "./build/server/index.js",
+
+            // Quan trọng: Báo PM2 đây là file binary, không phải JS thuần
+            interpreter: "none",
+
             env: {
                 PORT: 5188,
                 NODE_ENV: "production"
